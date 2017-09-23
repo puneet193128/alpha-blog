6dd624c1f1b0d066f8823d0b7ef3c46d0df247d8
Puneet Srivastva <srivastva.puneet@gmail.com>
2017-09-23 15:50:50 +0000

    Add asoociation between user and artile and add debugger view

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918042247) do
+ActiveRecord::Schema.define(version: 20170923063838) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "update_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
